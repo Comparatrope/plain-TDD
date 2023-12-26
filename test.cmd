6f69d4aefda5e73b12d11a209fd2a3f440658fcf
Comparatrope <148786411+Comparatrope@users.noreply.github.com>
2023-12-25 22:57:43 -0600

    Add files via upload

--- test.cmd
+++ test.cmd
@@ -1,57 +1,48 @@
 @SETLOCAL EnableDelayedExpansion & @ECHO OFF
-	SET "Fact=@Error_Level"
-	SET "Remark=Argument evaluates to !Fact!=0"
-	SET "Assertion=:assert.ErrorLevel"
-	SET "Act=!Assertion! Argument !Fact!"
-		SET "Argument=" & CALL :STAGE
-		SET "Argument=nul" & CALL :STAGE
-		SET "Argument=CALL nul" & CALL :STAGE
-		SET "Argument=FOR" & CALL :STAGE
-		SET "Argument=CALL FOR" & CALL :STAGE
-		SET "Argument=WRONG" & CALL :STAGE
-		SET "Argument=CALL WRONG" & CALL :STAGE
-		SET "Argument=1>2" & CALL :STAGE
-		SET "Argument=CALL 1>2" & CALL :STAGE
-		SET "Argument=vivid.cmd --random" & CALL :STAGE
-	SET "Fact=@vivid.STORE"
-	SET "Assertion=:assert.Readability"
-	SET "Remark=Argument evaluates to a readable output."
-	SET "Act=!Assertion! Argument"
-		SET "Argument=vivid.cmd --random" & CALL :STAGE
-		SET "Argument=vivid.cmd --random" & CALL :STAGE
-EXIT /B  %ErrorLevel%
-
-
-:STAGE & SETLOCAL
-	SET "THEME=vivid.cmd"
+	SET "STAGE=assert.cmd"
 	SET "LOG=test.cmd.log"
-	CALL !THEME! 100,100,255;0,0,0
-	SET "ID=%TIME::=%"
-		CALL !Act!
-	SET Error=%ErrorLevel%
-	IF %Error% EQU 0 SET "Status=PASS" & CALL !THEME! 100,255,100;0,0,0
-	IF %Error% NEQ 0 SET "Status=FAIL" & CALL !THEME! 255,100,100;0,0,0
-		SET Report=[!ID!]!Status!: !Remark!; actual !Fact!=!%Fact%!
-		ECHO !Report!
-		SET Argument
-		ECHO !Report! >> !LOG!
-		SET Argument >> !LOG!
-		SET Assertion >> !LOG!
-		SET !Fact! >> !LOG!
-		ECHO. >> !LOG!
-	CALL !THEME! --off
-EXIT /B %ErrorLevel%
+
+	SETLOCAL
+		SET "Input=vivid.cmd"
+		SET "Metric=assert#ErrorLevel.cmd"
+		SET "Remark=Input evaluates to ErrorLevel=0"
+			CALL !STAGE!
+	ENDLOCAL
+	SETLOCAL
+		SET "Input=vivid.cmd"
+		SET "Output=help"
+		SET "Metric=assert#stdOUT.cmd !Output!"
+		SET "Remark=Input evaluates to stdOUT with string '!Output!'"
+			CALL !STAGE!
+	ENDLOCAL
+
+	CALL :test.Faulty_Argument
+GOTO :EOF
 
 
-:assert.ErrorLevel
-	CMD /C "fromScript.cmd %~1"
-	SET "%~2=%ErrorLevel%"
-EXIT /B !%~2!
-
-
-:assert.Readability
-	CALL !%~1!
-	SET "Q=" & SET /P Q=[%~0] Input something if output is readable, or press ENTER
-	IF "%Q%" NEQ "" SET ErrorLevel=0
-EXIT /B  %ErrorLevel%
+:test.Faulty_Argument
+SETLOCAL
+	SET "Metric=assert#ErrorLevel.cmd"
+	SET "Remark=Input evaluates to ErrorLevel=0"
+	SETLOCAL
+		SET "Input=%*"
+		CALL !STAGE!
+	ENDLOCAL
+	SETLOCAL
+		SET "Input=%*CALL"
+		CALL !STAGE!
+	ENDLOCAL
+	SETLOCAL
+		SET "Input=%*NUL"
+		CALL !STAGE!
+	ENDLOCAL
+	SETLOCAL
+		SET "Input=%*EQU"
+		CALL !STAGE!
+	ENDLOCAL
+	SETLOCAL
+		SET "Input=%*IF"
+		CALL !STAGE!
+	ENDLOCAL
+GOTO :EOF
 
